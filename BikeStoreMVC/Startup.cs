77c76b3ebef9dd59c9c569861981d10fb0607da5
Simon Chettleburgh <simon_chettleburgh@hotmail.com>
2016-11-29 19:23:09 +0000

    Add project files.

---
+++ BikeStoreMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BikeStoreMVC.Startup))]
+namespace BikeStoreMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
